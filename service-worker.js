721bd5582fb05f480ac17e65e648e7cbd11b1182
Funanani Netshifhefhe <funifrank67@gmail.com>
2026-07-06 11:35:03 +0200

    improving caching of the web app

--- service-worker.js
+++ service-worker.js
@@ -6,20 +6,36 @@ const urlsToCache = [
   "./about.html",
   "./support.html",
   "./style.css",
-  "./script.js"
+  "./script.js",
+  "./manifest.json"
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Caching THUSO files...");
       return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
